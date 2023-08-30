d69c9c0ee3222c93f121d9283829a10857d3c5ec
spanricsi <span.ricsi@gmail.com>
2023-08-30 18:13:14 +0200

    neb-tag added

---
+++ components/neb-tag.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+defineEmits<{
+  remove: []
+}>()
+</script>
+
+<template>
+  <div class="neb-tag" @click.middle="$emit('remove')">
+    <slot />
+
+    <icon name="material-symbols:close-rounded" @click="$emit('remove')" />
+  </div>
+</template>
+
+<style scoped>
+.neb-tag {
+  display: flex;
+  align-items: center;
+  gap: var(--space-1);
+  width: fit-content;
+  padding: var(--space-1) var(--space-2);
+  border-radius: var(--radius-default);
+  border: 1px solid var(--neutral-color-200);
+  font-size: var(--text-sm);
+  font-weight: 500;
+  color: var(--neutral-color-700);
+
+  .icon {
+    width: 16px;
+    height: 16px;
+    color: var(--neutral-color-500);
+    cursor: pointer;
+    transition: color var(--duration-default);
+
+    &:hover {
+      color: var(--neutral-color-800);
+    }
+  }
+}
+
+.dark-mode {
+  .neb-tag {
+    border: 1px solid var(--neutral-color-700);
+    color: var(--neutral-color-300);
+
+    .icon {
+      color: var(--neutral-color-500);
+
+      &:hover {
+        color: var(--neutral-color-200);
+      }
+    }
+  }
+}
+</style>
